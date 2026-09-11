8f6fa96dfc3ba2f6fb11c058843e9545eb9262d8
Ezequiel Colunga Acha <ezequielcolungaacha@gmail.com>
2026-09-10 22:25:58 -0300

    fix for sasu.com.ar

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sasu.com.ar',
   vite: {
     plugins: [tailwindcss()],
-    base: "/sasu-store/"
-  }
+  },
 });
